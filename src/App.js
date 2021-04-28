2264c752c70184c56968f65cdaacc52233ec3f73
ishir0316.be20@chitkara,edu.in <ishir0316.be20@chitkara.edu.in>
2021-04-28 13:32:07 +0530

    temp

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import './App.css';
 import Header from "./components/Header";
 import Main from "./components/Main";
-
+import Wonder from "./components/Wonder";
 function App() {
   return (
     <div className="App">
       <Header/>
       <Main/>
+      <Wonder/>
       
     </div>
   );
---
+++ src/components/Wonder.js
@@ -0,0 +1,16 @@
+import "./Wonder.css";
+
+const Wonder=()=>{
+    return(
+        <div className="wonder">
+            <div className="wonder__img">
+                <img src="https://cdn.allbirds.com/image/upload/f_auto,q_auto/v1/production/pageSectionSecondaryPromoHero/en-US/images/1cRHg63d4UHUueFrqYwImu/1" alt=""/>
+                <div className="wonder__content">
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Wonder;
