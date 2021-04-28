f9145d8857bb25db3cf3b4c6c783a461b8f8f299
Inderpreet02 <inder192002@gmail.com>
2021-04-28 13:34:48 +0530

    buttons and fav section done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Favorites from './components/Favorites';
 
 function App() {
   return (
     <div className="App">
-      
+      <Favorites/>
     </div>
   );
 }
---
+++ src/components/FavCard.js
@@ -0,0 +1,23 @@
+import Btn from "./Btn";
+import "./FavCard.css"
+
+const FavCard = ( {heading, description, src, Icon, } ) => {
+    return (
+        <div className="favCard">
+            <img src={src} alt=""/>
+            <h2>{heading}</h2>
+            <div className="favCard__text">
+                <Icon className="icon__padding"/>
+                <p>{description}</p>
+            </div>
+            <div className="favorites__btn1">
+                <Btn text={true} text={"SHOP MEN"}/>
+            </div>
+            <div className="favorites__btn2">
+                <Btn text={true} text={"SHOP WOMEN"}/>
+            </div>
+        </div>
+    );
+}
+ 
+export default FavCard;
