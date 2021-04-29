6c6835b7e204f8ed4a3f500cb1410a12c98406ed
Inderpreet02 <inder192002@gmail.com>
2021-04-29 11:16:08 +0530

    almost done

--- src/components/FavCard.js
+++ src/components/FavCard.js
@@ -1,19 +1,32 @@
 import Btn from "./Btn";
 import "./FavCard.css"
 
-const FavCard = ( {heading, description, src, Icon, } ) => {
+const FavCard = ( {heading, description, src, Icon, className, func } ) => {
+    // const addShow = () =>{
+    //     document.querySelector(".favorites__btn1").classList.add("show");
+    //     document.querySelector(".favorites__btn1").classList.remove("hide");
+    //     document.querySelector(".favorites__btn2").classList.add("show");
+    //     document.querySelector(".favorites__btn2").classList.remove("hide");
+    // }
+
+    // const addHide = () => {
+    //     document.querySelector(".favorites__btn1").classList.remove("show");
+    //     document.querySelector(".favorites__btn1").classList.add("hide");
+    //     document.querySelector(".favorites__btn2").classList.remove("show");
+    //     document.querySelector(".favorites__btn2").classList.add("hide");
+    // }
     return (
-        <div className="favCard">
+        <div className={`favCard ${className}`}>
             <img src={src} alt=""/>
             <h2>{heading}</h2>
             <div className="favCard__text">
                 <Icon className="icon__padding"/>
                 <p>{description}</p>
             </div>
-            <div className="favorites__btn1">
+            <div className="favorites__btn2 ">
                 <Btn color={true} text={"SHOP MEN"}/>
             </div>
-            <div className="favorites__btn2">
+            <div className="favorites__btn1 ">
                 <Btn color={true} text={"SHOP WOMEN"}/>
             </div>
         </div>
--- src/components/Wonder.js
+++ src/components/Wonder.js
@@ -7,7 +7,8 @@ const Wonder=()=>{
                 <img src="https://cdn.allbirds.com/image/upload/f_auto,q_auto/v1/production/pageSectionSecondaryPromoHero/en-US/images/1cRHg63d4UHUueFrqYwImu/1" alt=""/>
                 <div className="wonder__content">
                     <h1>Supernatural Wonders</h1>
-                    <p>How did we create a super soft tee shirt that also
+                    <p>
+                        How did we create a super soft tee shirt that also
                         happens to reduce odor and keep you cool? With
                         nature,naturally.
                     </p>
